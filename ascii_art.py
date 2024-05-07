73225c3c7637566dafbc70d0e17a1336bbf9317f
skvch-a <skvch-al@gmail.com>
2024-05-07 15:58:47 +0500

    Add Mode Selection

--- ascii_art.py
+++ ascii_art.py
@@ -13,8 +13,10 @@ def resize_image(image, new_width, new_height):
     return resized_image
 
 
-def convert_to_ascii(image):
+def convert_to_ascii(image, inversion_mode):
     ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+    if inversion_mode:
+        ASCII_CHARS.reverse()
     grayscale_image = image.convert("L")
     pixels = grayscale_image.getdata()
     characters = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
@@ -27,14 +29,23 @@ def main():
         image = PIL.Image.open(path)
     except:
         print('Не удалось открыть изображение, возможно указан некорректный путь')
+        exit()
 
     try:
         new_width = int(input("Введите ширину ASCII_Art в символах (рекомендуется 100): "))
         new_height = int(input("Введите высоту ASCII_Art в символах (для автоподбора высоты введите 0): "))
     except:
         print('Некорректный ввод')
+        exit()
 
-    new_image_data = convert_to_ascii(resize_image(image, new_width, new_height)) 
+    mode_input = input("Выберите режим преобразования:\n1 - классический (рекомендуется для просмотра на светлом фоне)\n2 - инверсия (рекомендуется для просмотра на темном фоне)\n")
+    if (mode_input in ["1", "2"]):
+        inversion_mode = mode_input == "2"
+    else:
+        print('Некорректный ввод')
+        exit()
+
+    new_image_data = convert_to_ascii(resize_image(image, new_width, new_height), inversion_mode) 
     ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, len(new_image_data), new_width)])
     
     print(ascii_image)

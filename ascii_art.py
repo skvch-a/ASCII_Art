76a74228a4e1b8a8780852503765552fe935a306
skvch-a <skvch-al@gmail.com>
2024-05-07 16:11:15 +0500

    Refactoring

--- ascii_art.py
+++ ascii_art.py
@@ -22,28 +22,36 @@ def convert_to_ascii(image, inversion_mode):
     characters = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
     return(characters)    
 
-
-def main():
+def try_get_art_size():
     try:
-        path = input("Введите путь до изображения: ")
-        image = PIL.Image.open(path)
+        art_width = int(input("Введите ширину ASCII_Art в символах (рекомендуется 100): "))
+        art_height = int(input("Введите высоту ASCII_Art в символах (для автоподбора высоты введите 0): "))
+    except:
+        print('Некорректный ввод')
+        exit()
+    return (art_width, art_height)
+
+def try_open_image(path):
+    try:
+        return PIL.Image.open(path)
     except:
         print('Не удалось открыть изображение, возможно указан некорректный путь')
         exit()
 
-    try:
-        new_width = int(input("Введите ширину ASCII_Art в символах (рекомендуется 100): "))
-        new_height = int(input("Введите высоту ASCII_Art в символах (для автоподбора высоты введите 0): "))
-    except:
-        print('Некорректный ввод')
-        exit()
-
+def try_get_mode():
     mode_input = input("Выберите режим преобразования:\n1 - классический (рекомендуется для просмотра на светлом фоне)\n2 - инверсия (рекомендуется для просмотра на темном фоне)\n")
     if (mode_input in ["1", "2"]):
-        inversion_mode = mode_input == "2"
+        return mode_input == "2"
     else:
         print('Некорректный ввод')
         exit()
+
+def main():
+    path = input("Введите путь до изображения: ")
+    
+    image = try_open_image(path)
+    (new_width, new_height) = try_get_art_size()
+    inversion_mode = try_get_mode()
 
     new_image_data = convert_to_ascii(resize_image(image, new_width, new_height), inversion_mode) 
     ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, len(new_image_data), new_width)])

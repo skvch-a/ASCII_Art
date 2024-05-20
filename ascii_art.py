7f919ba0e0d549b34170b5f34fd95287acb5e1e3
skvch-a <skvch-al@gmail.com>
2024-05-20 21:54:07 +0500

    Refactoring

--- ascii_art.py
+++ ascii_art.py
@@ -5,39 +5,59 @@ import PIL.Image
 from argparse import ArgumentParser, RawTextHelpFormatter
 from tkinter import Tk, Label
 
-NAME = "ASCII Art Converter by Aleksey Sakevich"
-ASCII_CHARS = ["¶", "@", "#", "S", "%", "?", "*", "+", ";", ":", ",", ".", "`"]
+NAME = 'ASCII Art Converter by Aleksey Sakevich'
+ASCII_CHARS = ['¶', '@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.', '`']
 SYMBOL_RATIO = 2
 
-MODE_INPUT_MESSAGE = ("Режимы преобразования:\n"
-                      "1 - классический (рекомендуется для просмотра на светлом фоне)\n"
-                      "2 - инверсия (рекомендуется для просмотра на темном фоне)\n"
-                      "Выберите режим: ")
+MODE_INPUT_MESSAGE = ('Режимы преобразования:\n'
+                      '1 - классический (рекомендуется для просмотра на светлом фоне)\n'
+                      '2 - инверсия (рекомендуется для просмотра на темном фоне)\n'
+                      'Выберите режим: ')
 
-HELP_MESSAGE = ("ASCII Art Converter by Aleksey Sakevich\n\n"
-                "Консольное приложение, преобразующее изображение в ASCII Art\n"
-                "Поддерживаемые форматы - .PNG, .JPEG, .PPM, .GIF, .TIFF, .BMP\n"
-                "Результаты работы сохраняются в папке с этой программой\n\n"
-                "Пример запуска: ./ascii_art.py\n"
-                "Есть возможность передачи параметров сразу через командную строку\n"
-                "Пример (без автоподбора высоты): ./ascii_art.py --width=200 --height=200 --mode=1 --path=/path\n"
-                "Пример (с автоподбором высоты): ./ascii_art.py --width=200 --mode=1 --path=/path\n\n"
-                "Можно передавать параметры частично (например только --path),"
-                "тогда программа попросит ввести оставшиеся данные в консольном приложении.\n"
-                "Но если передать --width и не передавать --height, программа подберет высоту автоматически.\n"
-                "Если не передавать --width, программа попросит ввести размеры в приложении,"
-                "вне зависимости от наличия флага --height.")
+HELP_MESSAGE = ('ASCII Art Converter by Aleksey Sakevich\n\n'
+                'Консольное приложение, преобразующее изображение в ASCII Art\n'
+                'Поддерживаемые форматы - .PNG, .JPEG, .PPM, .GIF, .TIFF, .BMP\n'
+                'Результаты работы сохраняются в папке с этой программой\n\n'
+                'Пример запуска: ./ascii_art.py\n'
+                'Есть возможность передачи параметров сразу через командную строку\n'
+                'Пример (без автоподбора высоты): ./ascii_art.py --width=200 --height=200 --mode=1 --path=/path\n'
+                'Пример (с автоподбором высоты): ./ascii_art.py --width=200 --mode=1 --path=/path\n\n'
+                'Можно передавать параметры частично (например только --path),'
+                'тогда программа попросит ввести оставшиеся данные в консольном приложении.\n'
+                'Но если передать --width и не передавать --height, программа подберет высоту автоматически.\n'
+                'Если не передавать --width, программа попросит ввести размеры в приложении,'
+                'вне зависимости от наличия флага --height.')
 
+WIDTH_HELP_MESSAGE = 'Ширина для ASCII Art в символах'
+HEIGHT_HELP_MESSAGE = 'Высота для ASCII Art в символах'
+MODE_HELP_MESSAGE = 'Режим работы (1 - обычный, 2 - инверсия)'
+PATH_HELP_MESSAGE = 'Путь до изображения'
 
+PATH_INPUT_MESSAGE = 'Введите путь до изображения: '
+WIDTH_INPUT_MESSAGE = 'Введите ширину ASCII_Art в символах (рекомендуется 100 - 500): '
+HEIGHT_INPUT_MESSAGE = 'Введите высоту ASCII_Art в символах (для автоподбора высоты введите 0): '
+
+INPUT_ERROR_MESSAGE = 'Некорректный ввод'
+FILE_NOT_FOUND_ERROR_MESSAGE = 'Не удалось найти файл, возможно указан некорректный путь'
+INCORRECT_FORMAT_ERROR_MESSAGE = 'Некорретный формат файла'
+
+DEFAULT_VISUALIZER_FOREGROUND = 'black'
+DEFAULT_VISUALIZER_BACKGROUND = 'white'
+FONT = 'courier 4'
+
+def print_line():
+    print('-' * 100)
+    
+    
 def convert_to_ascii(image, inversion_mode):
     chars = ASCII_CHARS
     width = image.size[0]
     if inversion_mode:
         chars = list(reversed(ASCII_CHARS))
-    grayscale_image = image.convert("L")
+    grayscale_image = image.convert('L')
     pixels = grayscale_image.getdata()
-    art_data = "".join([chars[pixel // 20] for pixel in pixels])
-    result = "\n".join([art_data[line_start:line_start + width] for line_start in range(0, len(art_data), width)])
+    art_data = ''.join([chars[pixel // 20] for pixel in pixels])
+    result = '\n'.join([art_data[line_start:line_start + width] for line_start in range(0, len(art_data), width)])
     return result
 
 
@@ -54,19 +74,19 @@ def try_resize_image(image, width_from_args, height_from_args):
     try:
         if width_from_args == 0:
             print_line()
-            art_width = int(input("Введите ширину ASCII_Art в символах (рекомендуется 100 - 500): "))
-            art_height = int(input("Введите высоту ASCII_Art в символах (для автоподбора высоты введите 0): "))
+            art_width = int(input(WIDTH_INPUT_MESSAGE))
+            art_height = int(input(HEIGHT_INPUT_MESSAGE))
         else:
             art_width, art_height = width_from_args, height_from_args
         return resize_image(image, art_width, art_height)
     except ValueError:
-        sys.exit('Некорректный ввод')
+        sys.exit(INPUT_ERROR_MESSAGE)
 
 
 def try_get_path(path_from_args):
     if path_from_args == '':
         print_line()
-        path = input("Введите путь до изображения: ")
+        path = input(PATH_INPUT_MESSAGE)
     else:
         path = path_from_args
     return path
@@ -76,9 +96,9 @@ def try_get_image(path):
     try:
         return PIL.Image.open(path)
     except (FileNotFoundError, IsADirectoryError):
-        sys.exit('Не удалось найти файл, возможно указан некорректный путь')
+        sys.exit(FILE_NOT_FOUND_ERROR_MESSAGE)
     except PIL.UnidentifiedImageError:
-        sys.exit('Некорретный формат файла')
+        sys.exit(INCORRECT_FORMAT_ERROR_MESSAGE)
 
 
 def try_get_mode(mode_from_args):
@@ -88,41 +108,37 @@ def try_get_mode(mode_from_args):
     else:
         mode_input = mode_from_args
 
-    if mode_input in ["1", "2"]:
-        return mode_input == "2"
+    if mode_input in ['1', '2']:
+        return mode_input == '2'
     else:
-        sys.exit('Некорректный ввод')
+        sys.exit(INPUT_ERROR_MESSAGE)
 
 
 def visualize(content, inversion_mode):
-    foreground = "black"
-    background = "white"
+    foreground = DEFAULT_VISUALIZER_FOREGROUND
+    background = DEFAULT_VISUALIZER_BACKGROUND
     if inversion_mode:
         foreground, background = background, foreground
     window = Tk()
-    window.title("ASCII Art")
-    Label(window, text=content, anchor='w', font="courier 4", bg=background, fg=foreground).pack()
+    window.title(NAME)
+    Label(window, text=content, anchor='w', font=FONT, bg=background, fg=foreground).pack()
     window.mainloop()
 
 
 def save_result(ascii_art, original_image_path):
     source_filename = os.path.basename(original_image_path).split('.')[0]
-    result_filename = f"{source_filename}_ascii.txt"
-    with open(result_filename, "w") as f:
+    result_filename = f'{source_filename}_ascii.txt'
+    with open(result_filename, 'w') as f:
         f.write(ascii_art)
     print(f'Изображение сохранено по адресу {os.path.abspath(result_filename)}')
 
 
-def print_line():
-    print('-' * 100)
-
-
 def parse_cmd_args():
     parser = ArgumentParser(description=HELP_MESSAGE, formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--width', type=int, default=0, help='Ширина для ASCII Art в символах')
-    parser.add_argument('--height', type=int, default=0, help='Высота для ASCII Art в символах')
-    parser.add_argument('--mode', type=str, default='', help='Режим работы (1 - обычный, 2 - инверсия)')
-    parser.add_argument('--path', type=str, default='', help='Путь до изображения')
+    parser.add_argument('--width', type=int, default=0, help=WIDTH_HELP_MESSAGE)
+    parser.add_argument('--height', type=int, default=0, help=HEIGHT_HELP_MESSAGE)
+    parser.add_argument('--mode', type=str, default='', help=MODE_HELP_MESSAGE)
+    parser.add_argument('--path', type=str, default='', help=PATH_HELP_MESSAGE)
     return vars(parser.parse_args())
 
 
@@ -135,11 +151,11 @@ def main():
     image = try_get_image(path)
     resized_image = try_resize_image(image, args['width'], args['height'])
     inversion_mode = try_get_mode(args['mode'])
-
+    
     ascii_art = convert_to_ascii(resized_image, inversion_mode)
     save_result(ascii_art, path)
     visualize(ascii_art, inversion_mode)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
